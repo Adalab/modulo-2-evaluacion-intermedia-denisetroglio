6a7121329e07b596bc6b7a34223aba9557d0dfc0
denisetroglio <overbeck.denise@gmail.com>
2021-12-14 11:07:07 +0100

    add js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
 "use strict";
 
-const select = document.querySelector(".js_select");
+const selectOption = document.querySelector(".js_select");
 const playButn = document.querySelector(".js_button");
 const stone = document.querySelector(".js_stone");
 const paper = document.querySelector(".js_paper");
 const scissors = document.querySelector(".js_scissors");
 
 function getRandomNumber(max) { 
-    return Math.ceil(Math.random(10) * max); 
+  return Math.ceil(Math.random() * max); 
   }
+  console.log(getRandom(10));
 
 
-function playGame(){
+function playGame( ){
     console.log("ahora juego");
+    const userValue = selectOption.value; 
+
+if(getRandomNumber <3){
+
+}
+else if(getRandomNumber >=6){
+
+}
+else{
+
+}
 }
 
 function handleClickPlay(event){
@@ -20,5 +32,5 @@ function handleClickPlay(event){
     playGame();
 }
 
-
 playButn.addEventListener( 'click', handleClickPlay );
+
