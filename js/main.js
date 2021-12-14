51199e1c434ff19910af53703784a4de3ecafed3
denisetroglio <overbeck.denise@gmail.com>
2021-12-14 11:55:49 +0100

    add js changes

--- js/main.js
+++ js/main.js
@@ -5,20 +5,22 @@ const playButn = document.querySelector(".js_button");
 const stone = document.querySelector(".js_stone");
 const paper = document.querySelector(".js_paper");
 const scissors = document.querySelector(".js_scissors");
+const player = document.querySelector(".js_player");
+const computer = document.querySelector(".js_computer");
+
 
 function getRandomNumber(max) { 
   return Math.ceil(Math.random() * max); 
   }
-  console.log(getRandom(10));
 
 
 function playGame( ){
     console.log("ahora juego");
     const userValue = selectOption.value; 
-
 if(getRandomNumber <3){
 
 }
+
 else if(getRandomNumber >=6){
 
 }
@@ -30,6 +32,8 @@ else{
 function handleClickPlay(event){
     event.preventDefault();
     playGame();
+    const randomNumber = getRandomNumber(10);
+    console.log({randomNumber});
 }
 
 playButn.addEventListener( 'click', handleClickPlay );
