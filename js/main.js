447075164cccb78e4f8e42b5229e4f908d552dd9
denisetroglio <overbeck.denise@gmail.com>
2021-12-27 12:01:55 +0100

    adjustments evaluation JS

--- js/main.js
+++ js/main.js
@@ -1,40 +1,123 @@
 "use strict";
 
+const playButn = document.querySelector(".js_letsplay");
+const reloadBtn = document.querySelector(".js_reloadgame")
 const selectOption = document.querySelector(".js_select");
-const playButn = document.querySelector(".js_button");
-const stone = document.querySelector(".js_stone");
-const paper = document.querySelector(".js_paper");
-const scissors = document.querySelector(".js_scissors");
-const player = document.querySelector(".js_player");
+const resultInput = document.querySelector(".js_result");
 const computer = document.querySelector(".js_computer");
+const player = document.querySelector(".js_player");
 
+
+//variables globales:
+
+let playerScore = 0;
+let computerScore = 0;
+let moves = 0;
+
+//Funciones:
 
 function getRandomNumber(max) { 
   return Math.ceil(Math.random() * max); 
   }
 
 
-function playGame( ){
-    console.log("ahora juego");
-    const userValue = selectOption.value; 
-if(getRandomNumber <3){
+// general jugada aleatoria del ordenador:
+
+function generarJugada(){
+  let moveComputer = "";
+  let number = getRandomNumber(9);
+  if(number<=3){
+    moveComputer = "piedra";
+  } else if(number <= 6){
+    moveComputer = "papel";
+  }else{
+    moveComputer ="tijera";
+  }
+  return moveComputer;
+}
+
+//obtener la jugada de la usuaria
+
+function obtenerJugadaUser(){
+  return selectOption.value;
+}
+
+//comparar jugadas y ver quien ha ganado
+
+function playGame(){
+
+  let moveComputer = generarJugada();
+  let movePlayer = obtenerJugadaUser();
+  console.log(`jugada computadora ${moveComputer}`);
+ console.log(`jugada usuaria ${movePlayer}`);
+
+ moves++; // aumentar el numero de jugadas
+ console.log(moves);
+
+
+  if(moveComputer === movePlayer){
+    //empate
+    resultInput.innerHTML = "¡Empate!"
+
+  } else if(movePlayer === "piedra"){
+
+    if(moveComputer === "papel"){
+      resultInput.innerHTML = "¡Has perdido!"
+      computerScore++;
+
+    }else if(moveComputer === "tijera"){
+      resultInput.innerHTML = "¡Has ganado!"
+      playerScore++;
+    }
+
+  }else if(movePlayer === "tijera"){
+    if(moveComputer === "piedra"){
+      resultInput.innerHTML = "¡Has perdido!"
+      computerScore++;
+
+    }else if(moveComputer === "papel"){
+      resultInput.innerHTML = "¡Has ganado!"
+      playerScore++;
+    }
+
+  }else if(movePlayer === "papel"){
+    if(moveComputer === "tijera"){
+      resultInput.innerHTML = "¡Has perdido!"
+      computerScore++;
+
+    }else if(moveComputer === "piedra"){
+      resultInput.innerHTML = "¡Has ganado!"
+      playerScore++;
+    }
+ }
+ computer.innerHTML = "Ordenador: " + computerScore;
+ player.innerHTML = "Jugadora: " + playerScore;
 
 }
 
-else if(getRandomNumber >=6){
 
-}
-else{
+//finalizar juego:
 
+function gameOver(){
+  if(moves >=10){
+    if(playerScore > computerScore){
+      resultInput.innerHTML = "¡Has ganado el juego!"
+    }else if(playerScore < computerScore){
+      resultInput.innerHTML = "¡Has perdido el juego!"
+  }else{
+    resultInput.innerHTML = "¡Empate!"
+  }
 }
 }
+
+
+//mostrar resultador: texto, pontuación
 
 function handleClickPlay(event){
     event.preventDefault();
     playGame();
-    const randomNumber = getRandomNumber(10);
-    console.log({randomNumber});
+    gameOver();
 }
 
-playButn.addEventListener( 'click', handleClickPlay );
-
+//evento:
+playButn.addEventListener( "click", handleClickPlay);
